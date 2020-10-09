3f2dc80c75bacdcd70bcf2620f21c6be7480e0ff
alex <alexanderjhon997@gmail.com>
2020-10-08 22:02:40 -0500

    profe en este trabajo los realizamos entre eduin vergara franco y sergio muñoz zea

---
+++ punto3.py
@@ -0,0 +1,10 @@
+x=int(2)
+y=int(20)
+print(x)
+print(y)
+if x>y:
+ print(x,'es mayor que',y)
+elif x<y:
+ print(x,'es menor que',y)
+else:
+ print(x,'es igual a',y)
---
+++ punto5.py
@@ -0,0 +1,21 @@
+
+n1 = (input("ingrese la  el numero  1    "))
+
+n2 = (input("ingrese la  el numero  2   "))
+
+n3 = (input("ingrese la  el numero  3    "))
+
+
+
+if n1 < 0:
+    print("el numero es negativo y estos son los valores ",n1 ,n2, n3)
+elif n1 + n2 + n3:
+    print("el numero es positivo y esta es la suma", n1 + n2 + n3 )
+
+
+        
+            
+
+            
+ 
+    
