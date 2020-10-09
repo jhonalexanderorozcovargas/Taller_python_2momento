788b0893853449df4ce0e61d24c23c4f42e441a2
alex <alexanderjhon997@gmail.com>
2020-10-08 21:39:28 -0500

    subo los primeros ejercicios resueltos

---
+++ ejercicio1.py
@@ -0,0 +1,12 @@
+
+
+n1 = input("ingrese la  el numero  de la varianle A")
+
+n2 =  input("ingrese la  el numero  de la varianle B")
+
+num = n2 
+num2 = n1
+
+
+
+print ( num,num2)
---
+++ ejercicio4.py
@@ -0,0 +1,14 @@
+
+n1 = (input("ingrese la  el numero  1    "))
+
+n2 = (input("ingrese la  el numero  2   "))
+
+n3 = (input("ingrese la  el numero  3    "))
+
+if n2 < n1 > n3:
+    print("el mayor es el primero.numero",   n1)
+elif n1 < n2 > n3:
+    print("el mayor es el segundo.numero",   n2)
+elif n1 < n3 > n2:
+    print("el mayor es el tercero.numero",   n3)
+    
